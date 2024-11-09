3aac76c65ead09f57f2b36beaa119e0518ac3ad6
muhammad1301 <mianbrothers231@gmail.com>
2024-11-09 18:27:28 +0500

    if speed is 0 then function will run again

--- scraper.py
+++ scraper.py
@@ -10,10 +10,12 @@ class Speedtest(Selenium):
         print("Loading,Please Wait")
         time.sleep(15)
         try:
+            self.wait.until(EC.presence_of_element_located((By.XPATH, '//div[@id="speed-value"]')))
             speed = self.text(By.XPATH,'//div[@id="speed-value"]')
             speed_unit = self.text(By.XPATH,'//div[@id="speed-units"]')
-            print(speed,speed_unit)
+            if speed == 0:
+                print("Try again..")
+            else:
+                print(speed,speed_unit)
         except:
-            print("Your internet is too Slow")
-
-
+            print("Your internet is too Slow..")
